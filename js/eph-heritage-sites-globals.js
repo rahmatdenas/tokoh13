e478b6aae061dcbac6eb88610c1afcb761971f39
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-05-29 16:46:59 +0700

    Update eph-heritage-sites-globals.js

--- js/eph-heritage-sites-globals.js
+++ js/eph-heritage-sites-globals.js
@@ -7,23 +7,20 @@ const KAMUS_PEKERJAAN = {
   'Q82955': 'Politikus', 'Q13141064': 'Pemain Bulu Tangkis', 
   'Q177220': 'Penyanyi', 'Q937857': 'Pemain Sepak Bola', 
   'Q4610556': 'Peragawan', 'Q1930187': 'Jurnalis', 
-  'Q3665646': 'Pemain Basket', 'Q193391': 'Diplomat', 
-  'Q189459': 'Ulama', 'Q1028181': 'Pelukis', 'Q39631': 'Dokter',
-  
+  'Q193391': 'Diplomat', 'Q189459': 'Ulama', 
+  'Q1028181': 'Pelukis', 'Q39631': 'Dokter',
   // KELOMPOK PEGAWAI NEGERI
   'Q212238': 'Pegawai Negeri', 'Q572700': 'Pegawai Negeri',
-  
   // KELOMPOK PEMERAN
   'Q33999': 'Pemeran', 'Q10800557': 'Pemeran', 'Q10798782': 'Pemeran',
-  
   // KELOMPOK MILITER
-  'Q470647': 'Militer', 'Q189290': 'Militer',
+  'Q47064': 'Militer', 'Q189290': 'Militer',
   
   // KELOMPOK PENULIS
-  'Q36180': 'Penulis', 'Q6625963': 'Penulis',
+  'Q36180': 'Penulis', 
   
   // KELOMPOK PRAKTISI HUKUM
-  'Q40348': 'Praktisi Hukum', 'Q16533': 'Praktisi Hukum', 'Q600751': 'Praktisi Hukum',
+  'Q40348': 'Praktisi Hukum', 'Q16533': 'Praktisi Hukum', 'Q600751': 'Praktisi Hukum',  'Q189010': 'Praktisi Hukum',
   
   // KELOMPOK POLISI
   'Q384593': 'Polisi', 'Q35535': 'Polisi',
@@ -37,15 +34,22 @@ const KAMUS_PEKERJAAN = {
   // KELOMPOK AKADEMISI / DOSEN
   'Q1622272': 'Akademisi/Dosen', 'Q1650915': 'Akademisi/Dosen', 
   'Q1569495': 'Akademisi/Dosen', 'Q462390': 'Akademisi/Dosen',
-  'Q121594': 'Akademisi/Dosen', 'Q3400985': 'Akademisi/Dosen', 'Q901': 'Akademisi/Dosen',  
+  'Q121594': 'Akademisi/Dosen', 'Q3400985': 'Akademisi/Dosen', 
+  'Q901': 'Akademisi/Dosen',  'Q114316380': 'Akademisi/Dosen',
   
   // KELOMPOK MUSISI
   'Q753110': 'Musisi', 'Q639669': 'Musisi', 'Q36834': 'Musisi',  
   
   // KELOMPOK SINEAS
   'Q2526255': 'Sineas', 'Q28389': 'Sineas', 'Q3282637': 'Sineas',
+
+  // KELOMPOK SENIMAN LAINNYA
+  'Q483501': 'Seniman Lainnya',
+  'Q5716684': 'Seniman Lainnya',
+  'Q6625963': 'Seniman Lainnya',
   
   // KELOMPOK ATLET LAINNYA
+  'Q3665646': 'Atlet Lainnya',
   'Q2066131': 'Atlet Lainnya', 'Q11513337': 'Atlet Lainnya', 'Q11338576': 'Atlet Lainnya', 
   'Q10833314': 'Atlet Lainnya', 'Q58825429': 'Atlet Lainnya', 'Q2309784': 'Atlet Lainnya'
 };
